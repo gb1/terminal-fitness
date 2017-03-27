71815127de32f5f8081960b171e5abf9d22012f3
Gregor Brett <gregor.brett@bluefinsolutions.com>
2017-03-27 23:10:32 +0100

    move pushups out to separate loop

--- lib/server.ex
+++ lib/server.ex
@@ -10,9 +10,7 @@ defmodule TerminalFitness.Server do
   def accept(listening_socket, handler) do
     IO.puts "accept"
     socket = Socket.TCP.accept!(listening_socket)
-    # socket |> Socket.Stream.send!(IO.ANSI.blue_background())
     socket |> Socket.Stream.send!(welcome())
-    # socket |> Socket.Stream.send!(IO.ANSI.green_background())
     socket |> Socket.Stream.send!(options())
 
     spawn (fn -> handle(socket, handler) end)
@@ -23,16 +21,22 @@ defmodule TerminalFitness.Server do
     IO.puts "handle"
 
     case Socket.Stream.recv(socket) do
-      # {:ok, nil} ->
-      #   socket |> Socket.Stream.shutdown
+      {:ok, nil} ->
+        socket |> Socket.Stream.shutdown
       {:ok, "quit\r\n"} ->
         socket |> Socket.Stream.shutdown
+
+      {:ok, "1\r\n"} ->
+          pushups(socket)
+
       {:ok, incoming} ->
         IO.inspect incoming
         socket |> Socket.Stream.send!(handler.(incoming))
         handle(socket, handler)
+
       {:error, _incoming} ->
         socket |> Socket.Stream.shutdown
+
     end
   end
 
@@ -40,7 +44,7 @@ defmodule TerminalFitness.Server do
     cond do
       line =~ ~r/quit/ -> quit()
       line =~ ~r/help/ -> options()
-      line =~ ~r/1/ -> pushups()
+      # line =~ ~r/1/ -> pushups()
       line =~ ~r/2/ -> squats()
       line =~ ~r/3/ -> plank()
       true ->
@@ -74,9 +78,27 @@ defmodule TerminalFitness.Server do
     "Choose 1-3: " <> IO.ANSI.white()
   end
 
-  def pushups do
-    IO.ANSI.clear <>
-    TerminalFitness.PushUps.greeting()
+  def pushups(socket, week, day) do
+    socket |> Socket.Stream.send!(
+      "ok, do this..."
+    )
+    handle(socket, &handler/1)
+  end
+  def pushups(socket, week) do
+
+    socket |> Socket.Stream.send!(
+      "OK, what day are you on?: (1-3) "
+    )
+    day = Socket.Stream.recv(socket)
+    pushups(socket, week, day)
+  end
+  def pushups(socket) do
+    socket |> Socket.Stream.send!(
+      IO.ANSI.clear <>
+      TerminalFitness.PushUps.greeting()
+    )
+    week = Socket.Stream.recv(socket)
+    pushups(socket, week)
   end
 
   def squats do
